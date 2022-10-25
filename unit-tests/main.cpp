6fbb55eaa4ccf6747d44ee04456d6a84b49625d9
Valentin Nikin <nikin_valentin@mail.ru>
2022-10-26 00:45:28 +0300

    Implemented home work 3

--- unit-tests/main.cpp
+++ unit-tests/main.cpp
@@ -1,9 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(Example, Test1) {
-    ASSERT_EQ(10, 12);
-}
-
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
